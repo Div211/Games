1a0e1f4fb2bd63fc954eb12926a7d93527fda155
Divyanshu Mittal <94476659+Div211@users.noreply.github.com>
2024-08-16 00:01:29 +0530

    Update project 1.java

--- project 1.java
+++ project 1.java
@@ -2,8 +2,8 @@
 import java.util.Scanner;
 import java.util.Random;
 //Code for rock paper scissor
-project 1
-Code for rock paper scissor
+//project 1
+//Code for rock paper scissor
 public class projects {    
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
